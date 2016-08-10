044b6fa677ae95c922add4e7088c15cd84602f2b
Ameya <ameya.kulkarni@weboniselab.com>
2016-08-10 17:55:22 +0530

    Changes for adding the square using range

--- ranges/main.go
+++ ranges/main.go
@@ -6,12 +6,28 @@ import (
 
 func main() {
 	fmt.Println("lets see some ranges")
-	nums := []int{1, 2, 3, 4, 5}
+  nums := make([]int, 5)
+
+  nums[0] = 1
+  nums[1] = 2
+  nums[2] = 3
+  nums[3] = 4
+  nums[4] = 5
 	sum := 0
 
 	for _, element := range nums {
 		sum += element
 	}
 
-	println("sum of all elements using range is :", sum)
+  nums_squared := make([]int, 5)
+  fmt.Println(len(nums_squared))
+	fmt.Println("sum of all elements using range is :", sum)
+
+  fmt.Println("now lets square every element of the num array")
+  fmt.Println(len(nums_squared))
+  for index,element := range nums{
+      nums_squared[index] = element**2
+  }
+  fmt.Println(nums_squared)
+
 }
