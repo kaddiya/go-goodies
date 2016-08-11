cfaeaed33725e6a1262fb6429a1cc3ca307242b3
Ameya <ameya.kulkarni@weboniselab.com>
2016-08-11 16:23:14 +0530

    Changes for having a method in place

--- structs/main.go
+++ structs/main.go
@@ -5,13 +5,9 @@ import(
   alias_m "github.com/kaddiya/go-goodies/structs/models"
 )
 
-type Person struct{
-  name string
-  age int
-}
 func main(){
   fmt.Println("lets see some structs in action")
-   a := alias_m.Employee{1}
-  fmt.Println(alias_m.OuterPerson{"ameya",20,true})
-  fmt.Println(a)
+  // employee := alias_m.Employee{1}
+  outer_person := alias_m.OuterPerson{"ameya-2",20,true}
+  outer_person.Talk()
 }
--- structs/models/person.go
+++ structs/models/person.go
@@ -1,7 +1,15 @@
 package models
 
+import(
+  "fmt"
+)
+
 type OuterPerson struct{
   Name string
   Age int
   IsAdmin bool
+}
+
+func (ptr *OuterPerson) Talk() {
+  fmt.Println("Hello,I am "+ptr.Name)
 }
